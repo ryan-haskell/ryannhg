bec6aa6f750def6d924adf4c9c479e3bbdba6b67
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2018-10-09 21:56:30 -0500

    new site, new life

--- src/Main.elm
+++ src/Main.elm
@@ -1,85 +1,483 @@
-module Main exposing (main)
+port module Main exposing (main)
 
+import Browser exposing (Document, UrlRequest(..))
+import Browser.Navigation as Nav exposing (Key)
+import DateFormat
 import Element exposing (..)
-import Elements
-import Navigation exposing (Location)
-import Pages.Home
-import Pages.NotFound
-import Pages.Projects
-import Pages.ThoughtDetail
-import Pages.Thoughts
-import Routes
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Region as Region
+import Html exposing (Html)
+import Html.Attributes as Attr
+import Markdown
 import Task
-import Types exposing (Page(..))
-import Window
+import Time exposing (Zone)
+import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>), s, string)
 
 
-main : Program Never Model Msg
-main =
-    Navigation.program
-        HandleLocation
-        { init = init
-        , view = view >> layout []
-        , update = update
-        , subscriptions = always (Window.resizes OnResize)
-        }
+type alias Flags =
+    ()
 
 
 type alias Model =
-    { location : Location
-    , device : Maybe Element.Device
+    { url : Url
+    , key : Key
+    , timezone : Zone
     }
 
 
 type Msg
-    = HandleLocation Location
-    | OnResize Window.Size
+    = OnUrlRequest UrlRequest
+    | OnUrlChange Url
+    | SetTimezone Zone
+
+
+type Page
+    = Homepage
+    | BlogDetail Post
+    | NotFound
+
+
+main : Program Flags Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        , onUrlRequest = OnUrlRequest
+        , onUrlChange = OnUrlChange
+        }
+
+
+init : Flags -> Url -> Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( Model url key Time.utc
+    , getTimezone
+    )
+
+
+getTimezone : Cmd Msg
+getTimezone =
+    Task.perform SetTimezone Time.here
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        HandleLocation location ->
-            { model | location = location } ! []
+        SetTimezone zone ->
+            ( { model | timezone = zone }
+            , Cmd.none
+            )
 
-        OnResize size ->
-            { model | device = Just (Element.classifyDevice size) } ! []
+        OnUrlRequest urlRequest ->
+            case urlRequest of
+                Internal url ->
+                    ( model
+                    , Nav.pushUrl model.key (Url.toString url)
+                    )
+
+                External url ->
+                    ( model
+                    , outgoing url
+                    )
+
+        OnUrlChange url ->
+            ( { model | url = url }
+            , outgoing "URL_CHANGE"
+            )
 
 
-view : Model -> Element Msg
+port outgoing : String -> Cmd msg
+
+
+pageTitle : Url -> String
+pageTitle url =
+    String.join " | " <|
+        case pageFromUrl url of
+            Homepage ->
+                [ "Ryan Haskell-Glatz" ]
+
+            BlogDetail post ->
+                [ post.title, "Blog", "Ryan Haskell-Glatz" ]
+
+            NotFound ->
+                [ "Not Found", "Ryan Haskell-Glatz" ]
+
+
+view : Model -> Document msg
 view model =
-    case model.device of
-        Just device ->
-            column
-                Elements.pageStyles
-                [ Elements.navbar device (Routes.page model.location)
-                , el [ height fill, width fill ] (viewPage device model)
-                , Elements.footer device
-                ]
-
-        Nothing ->
-            empty
+    { title = pageTitle model.url
+    , body = [ Element.layout [] (page model) ]
+    }
 
 
-viewPage : Device -> Model -> Element Msg
-viewPage device model =
-    case Routes.page model.location of
+colors =
+    { white = rgb 1 1 1
+    , blue = rgb255 0 100 175
+    , darkBlue = rgb255 0 125 200
+    , lightGray = rgb 0.9 0.9 0.9
+    , darkGray = rgb 0.3 0.3 0.3
+    }
+
+
+sourceSansPro =
+    Font.family
+        [ Font.typeface "Source Sans Pro"
+        ]
+
+
+page : Model -> Element msg
+page model =
+    case pageFromUrl model.url of
         Homepage ->
-            Pages.Home.view device
+            homepage model
 
-        Projects ->
-            Pages.Projects.view device
-
-        Thoughts ->
-            Pages.Thoughts.view device
-
-        ThoughtDetail thought ->
-            Pages.ThoughtDetail.view device thought
+        BlogDetail post ->
+            blogDetailPage model post
 
         NotFound ->
-            Pages.NotFound.view
+            notFoundPage
 
 
-init : Location -> ( Model, Cmd Msg )
-init location =
-    ( Model location Nothing, Task.perform OnResize Window.size )
+pageFromUrl : Url -> Page
+pageFromUrl url =
+    case Parser.parse route url of
+        Just page_ ->
+            page_
+
+        Nothing ->
+            NotFound
+
+
+route : Parser.Parser (Page -> a) a
+route =
+    Parser.oneOf
+        [ Parser.map Homepage Parser.top
+        , Parser.map BlogDetail (s "blog" </> Parser.custom "POST" postFromSlug)
+        ]
+
+
+postFromSlug : String -> Maybe Post
+postFromSlug slug =
+    posts
+        |> List.filter (\post -> post.slug == slug)
+        |> List.head
+
+
+homepage : Model -> Element msg
+homepage model =
+    column
+        [ width fill, sourceSansPro ]
+        [ navbar
+        , hero
+            { title = "ryan."
+            , caption = "i like coding things!"
+            }
+        , latestPosts model.timezone
+        , footer
+        ]
+
+
+notFoundPage : Element msg
+notFoundPage =
+    column
+        [ width fill, sourceSansPro ]
+        [ navbar
+        , hero { title = "page not found.", caption = "sorry about that!" }
+        , footer
+        ]
+
+
+blogDetailPage : Model -> Post -> Element msg
+blogDetailPage { timezone } { title, time, content } =
+    column
+        [ width fill, sourceSansPro ]
+        [ navbar
+        , hero { title = title, caption = format timezone time }
+        , el containerStyles <|
+            textColumn
+                [ Element.htmlAttribute (Attr.class "markdown") ]
+                (List.map Element.html (Markdown.toHtml Nothing content))
+        , footer
+        ]
+
+
+navbar : Element msg
+navbar =
+    el
+        [ width fill
+        , Background.color colors.blue
+        , Font.color colors.white
+        , paddingXY 0 16
+        ]
+    <|
+        row containerStyles
+            [ brand
+            , el [ alignRight ] <|
+                row [ spacing 12, Font.size 16 ] <|
+                    List.map
+                        (\name ->
+                            link
+                                navLinkStyles
+                                { url = "/" ++ name, label = text name }
+                        )
+                        pages
+            ]
+
+
+navLinkStyles =
+    [ Font.underline
+    ]
+
+
+pages =
+    [--     "about"
+     -- , "blog"
+     -- , "projects"
+    ]
+
+
+containerStyles =
+    [ width (fill |> maximum 720)
+    , centerX
+    , paddingXY 16 0
+    ]
+
+
+brand =
+    link [ Font.semiBold, Font.size 24 ]
+        { url = "/"
+        , label = text "ryan."
+        }
+
+
+hero : { a | title : String, caption : String } -> Element msg
+hero { title, caption } =
+    el
+        [ Background.color colors.blue
+        , Background.gradient
+            { steps = [ colors.darkBlue, colors.blue ]
+            , angle = 0
+            }
+        , Font.color colors.white
+        , width fill
+        , paddingXY 0 128
+        ]
+    <|
+        textColumn ([ spacing 12 ] ++ containerStyles)
+            [ paragraph
+                [ Font.size 64
+                , Font.semiBold
+                , Region.heading 1
+                ]
+                [ text title ]
+            , paragraph
+                [ Font.size 28
+                , Region.heading 2
+                ]
+                [ text caption ]
+            ]
+
+
+type alias Post =
+    { title : String
+    , slug : String
+    , tags : List String
+    , time : Time.Posix
+    , content : String
+    }
+
+
+posts =
+    [ Post
+        "New site, new life!"
+        "new-site-new-life"
+        [ "elm", "web", "project" ]
+        (Time.millisToPosix 1539137597759)
+        """
+## Elm is neat.
+
+I just built this entire site with it! Alright, I guess there's a bit of HTML, CSS, and JS too. But that's just the glue to attach the Elm to everything.
+
+_The coolest part?_ I built all of this with almost zero CSS! Using the `elm-ui` package, I was able to layout this page in terms of normal things, like "rows" and "columns". Not "divs" and "spans".
+
+Let's take a look at what the homepage hero looks like in Elm UI:
+
+```elm
+hero =
+    column
+        [ padding 32
+        , Font.color white
+        , Background.color blue
+        ]
+        [ text "ryan."
+        , text "i like coding things!"
+        ]
+```
+
+In Elm UI, when I want things to stack I can use a `column`. When I want things side-by-side, I use a `row`. Behind the scenes, everything compiles to the cross-browser supported CSS.
+
+No more debugging an IE 11 CSS issues!
+
+On top of that, it's really easy to keep track of colors used across the site because you can share variables between Elm (unlike variables across HTML, JS and CSS).
+
+```elm
+white : Color
+white =
+    rgb 1 1 1
+
+blue : Color
+blue =
+    rgb255 0 100 175
+
+hero : Element a
+hero =
+    column
+        [ padding 32
+        , Font.color white
+        , Background.color blue
+        ]
+        [ text "ryan."
+        , text "i like coding things!"
+        ]
+```
+
+With elm, making something like `hero` a partial that can take in different kinds of inputs is super-easy too. This is because everything in Elm is already a function. We can pass in the title and caption as string inputs!
+
+
+```elm
+white : Color
+white =
+    rgb 1 1 1
+
+blue : Color
+blue =
+    rgb255 0 100 175
+
+type alias HeroContent =
+    { title : String
+    , caption : String
+    }
+
+hero : HeroContent -> Element a
+hero content =
+    column
+        [ padding 32
+        , Font.color white
+        , Background.color blue
+        ]
+        [ text content.title
+        , text content.caption
+        ]
+
+homepageHero : Element a
+homepageHero =
+    hero
+        { title = "ryan."
+        , caption = "i like coding things!"
+        }
+
+blogDetailHero : BlogPost -> Element a
+blogDetailHero post =
+    hero
+        { title = post.title
+        , caption = post.date
+        }
+```
+
+If you want to learn more, check out [the repo here](https://github.com/mdgriffith/elm-ui)! Building websites without CSS is a great way to learn Elm's non-C-style syntax!
+
+"""
+    ]
+
+
+latestPosts : Zone -> Element msg
+latestPosts timezone =
+    el ([] ++ containerStyles) <|
+        textColumn [ paddingXY 0 64, spacing 36 ]
+            [ el [ Region.heading 3, Font.size 48, Font.semiBold ] (text "Latest posts")
+            , column [ spacing 24 ] (List.map (viewPostListing timezone) posts)
+            ]
+
+
+viewPostListing : Zone -> Post -> Element msg
+viewPostListing timezone post =
+    link
+        [ paddingXY 32 16
+        , width fill
+        , Border.width 1
+        , Border.rounded 4
+        , Border.color colors.lightGray
+        , mouseOver
+            [ Border.shadow
+                { offset = ( 0, 4 )
+                , size = 2
+                , blur = 8
+                , color = colors.lightGray
+                }
+            ]
+        , Element.htmlAttribute
+            (Attr.style
+                "transition"
+                "box-shadow 300ms ease-in-out"
+            )
+        ]
+        { url = "/blog/" ++ post.slug
+        , label =
+            column [ paddingXY 0 16, spacing 8 ]
+                [ el
+                    [ Font.size 24
+                    , Font.semiBold
+                    ]
+                    (text post.title)
+                , el [ Font.color colors.darkGray ] (text (format timezone post.time))
+                ]
+        }
+
+
+format : Zone -> Time.Posix -> String
+format zone =
+    DateFormat.format
+        [ DateFormat.monthNameFull
+        , DateFormat.text " "
+        , DateFormat.dayOfMonthSuffix
+        , DateFormat.text ", "
+        , DateFormat.yearNumber
+
+        -- , DateFormat.text " at "
+        -- , DateFormat.hourNumber
+        -- , DateFormat.text ":"
+        -- , DateFormat.minuteFixed
+        -- , DateFormat.amPmLowercase
+        ]
+        zone
+
+
+linkStyles =
+    [ Font.color colors.blue
+    , mouseOver
+        [ alpha 0.75
+        ]
+    ]
+
+
+footer : Element msg
+footer =
+    el containerStyles <|
+        row
+            [ Font.size 16
+            , paddingXY 0 32
+            , width fill
+            ]
+            [ text "Built by Ryan, made with "
+            , newTabLink linkStyles { url = "https://elm-lang.org", label = text "Elm!" }
+            , row [ spacing 16, alignRight ]
+                [ newTabLink linkStyles
+                    { url = "https://www.github.com/ryannhg", label = text "Github" }
+                , newTabLink linkStyles
+                    { url = "https://www.twitter.com/ryan_nhg", label = text "Twitter" }
+                ]
+            ]
